f4af7f4d70731b1ce9e0b845744e30ad96e0757d
inavarro96 <isnavest2@gmail.com>
2022-10-05 05:39:57 -0500

    Se agrega mpdulo para terminar el pedido.

--- app/model/PedidoModel.php
+++ app/model/PedidoModel.php
@@ -24,29 +24,28 @@ class PedidoModel extends Connection {
     }
 
     public function insert($pedido) {
-        $stm = Connection::connect() -> prepare("INSERT INTO pedido(nombre_completo, direccion, telefono, visto, atendido, fecha_creado, fecha_eliminado)
-        VALUES (:nombre_completo, :direccion, :telefono, :visto, :atendido, :fecha_creado, NULL)");
-
+        $stm = Connection::connect() -> prepare("INSERT INTO pedido(nombre_completo, direccion, telefono, visto, atendido, fecha_eliminado)
+        VALUES (:nombre_completo, :direccion, :telefono, :visto, :atendido, NULL)");
+        $atendido =0;
+        $visto = 0;
         $stm -> bindParam("nombre_completo",$pedido['nombre_completo'], PDO::PARAM_STR);
         $stm -> bindParam("direccion",$pedido['direccion'], PDO::PARAM_STR);
         $stm -> bindParam("telefono",$pedido['telefono'], PDO::PARAM_STR);
-        $stm -> bindParam("visto",$pedido['visto'], PDO::PARAM_STR);
-        $stm -> bindParam("fecha_creado",$pedido['fecha_creado'], PDO::PARAM_STR);
-        $stm -> bindParam("usuario",$pedido['usuario'], PDO::PARAM_STR);
-
+        $stm -> bindParam("visto",$visto, PDO::PARAM_INT);
+        $stm -> bindParam("atendido",$atendido, PDO::PARAM_INT);
         $stm -> execute();
 
-        $stm2 = Connection::connect() -> prepare("SELECT last_insert_id() AS id");
+        $stm2 = Connection::connect() -> prepare("SELECT MAX(id) as id FROM pedido;");
         $stm2 -> execute();
         $idPedido = $stm2->fetch(PDO::FETCH_ASSOC);
         $res = null;
         foreach($pedido['productos'] as  &$producto) {
-            
+            print_r("pedido ".$idPedido['id']);
             $stm3 =  Connection::connect() -> prepare("INSERT INTO producto_pedido(id_pedido, id_producto, cantidad)
             VALUES (:id_pedido, :id_producto, :cantidad)");
-            $stm3 -> bindParam("id_pedido",$producto['id_pedido'], PDO::PARAM_STR);
-            $stm3 -> bindParam("id_producto",$idPedido['id_producto'], PDO::PARAM_STR);
-            $stm3 -> bindParam("cantidad",$idPedido['cantidad'], PDO::PARAM_STR);
+            $stm3 -> bindParam("id_pedido",$idPedido['id'], PDO::PARAM_STR);
+            $stm3 -> bindParam("id_producto",$producto['id_producto'], PDO::PARAM_STR);
+            $stm3 -> bindParam("cantidad",$producto['cantidad'], PDO::PARAM_STR);
             $res =  $stm3 -> execute();
         }
         if($res){
